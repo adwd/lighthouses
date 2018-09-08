239dc417d006cca553fc6331f0e93d83a0da3ca8
adwd <ecdt.mn@gmail.com>
2018-09-08 19:56:27 +0900

    add primer

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -10,15 +10,14 @@ export class AppRoot {
   render() {
     return (
       <div>
-        <header>
-          <h1>Stencil App Starter</h1>
-        </header>
+        <div class="Subhead">
+          <div class="Subhead-heading">Light houses</div>
+        </div>
 
         <main>
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
               <stencil-route url='/' component='app-home' exact={true} />
-              <stencil-route url='/profile/:name' component='app-profile' />
             </stencil-route-switch>
           </stencil-router>
         </main>
